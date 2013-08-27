a2046be9d246b43e418afab16110f8553d1ff65b
Norrisa <norris.andrewj@gmail.com>
2013-08-27 11:29:10 +0100

    update

--- run_sed.py
+++ run_sed.py
@@ -1,53 +1,95 @@
 from sediment import *
 import libspud
 import sys
+import argparse
 
 #should be an osml file
-try: 
-  libspud.load_options('test15.osml')
-except:
-	print "This file doesn't exist or is the wrong file type. It should be a .osml file"
-	sys.exit()
 
-try:
-	start = float(libspud.get_option('/diffusion_model/timestepping/start_time',))
-	end = float(libspud.get_option('/diffusion_model/timestepping/end_time'))
-	time_step = float(libspud.get_option('/diffusion_model/timestepping/timestep'))
-	mesh_int = int(libspud.get_option('/diffusion_model/mesh/initial_mesh_size'))
-	alpha = float(libspud.get_option('/diffusion_model/model_parameters/diffusion_coefficient'))
-	initial_conditions = str(libspud.get_option('/diffusion_model/model_parameters/initial_conditions'))
-except: 
-	print 'The information provided was incomplete, please recreate the file'
-	sys.exit()
+
+def Main():
+	#Import the command line arguments and return them
+	parser = argparse.ArgumentParser(description='input a file name')
+	parser.add_argument('--v','--verbose', action='store_true', help="Verbose output: mainly graphics at each time step", default=False)
+	parser.add_argument('file_name', help='Insert the file name of the simulation you wish to run. It should end with.osml')
+	args = parser.parse_args()
+	libspud.clear_options()
+	file_name = args.file_name
+	Verbose = args.v
+
+	return file_name, Verbose
+
+def Parameters(file_name):
 	
-if ((start-end) >= 0):
-	print 'The start time is after the end time'
-	sys.exit()
-elif (time_step == 0):
-	print 'The time step cannot be 0'
-	sys.exit()
-elif (mesh_int == 0):
-	print 'The mesh has to have a size greater than 0'
-	sys.exit()
-elif ((end - start) <= time_step):
-	print 'The time step is too large for the total time'
-	sys.exit()
+	try: 
+		libspud.load_options(file_name)
+	except:
+		print "This file doesn't exist or is the wrong file type. It should be a .osml file. Use -h for help"
+		sys.exit()
+		
+	try:
+		start = float(libspud.get_option('/diffusion_model/timestepping/start_time',))
+		end = float(libspud.get_option('/diffusion_model/timestepping/end_time'))
+		time_step = float(libspud.get_option('/diffusion_model/timestepping/timestep'))
+		mesh_int = int(libspud.get_option('/diffusion_model/mesh/initial_mesh_size'))
+		alpha = float(libspud.get_option('/diffusion_model/model_parameters/diffusion_coefficient'))
+		initial_conditions = str(libspud.get_option('/diffusion_model/model_parameters/initial_conditions'))
+	except: 
+		print 'The information provided was incomplete, please recreate the file'
+		sys.exit()
 	
-#create a simple testcase
-model = SedimentModel()
-mesh = UnitSquareMesh(mesh_int,mesh_int)
-model.set_mesh(mesh)
-init_cond = Expression(initial_conditions) # simple slope
-init_sed = Expression('x[0]') # this gives
-# total of above gives a slope of 0 to 2 (over the unit square)
-model.set_initial_conditions(init_cond,init_sed)
-#model.set_start_time(start)
-model.set_timestep(time_step)
-model.set_end_time(end)
-model.set_diffusion_coeff(alpha)
-model.init()
-model.solve()
-# answer should be 1 everywhere
-plot(model.get_total_height(),interactive=True)
-print model.get_total_height_array()
+	if ((start-end) >= 0):
+		print 'The start time is after the end time'
+		sys.exit()
+	elif (time_step == 0):
+		print 'The time step cannot be 0'
+		sys.exit()
+	elif (mesh_int == 0):
+		print 'The mesh has to have a size greater than 0'
+		sys.exit()
+	elif ((end - start) < time_step):
+		print 'The time step is too large for the total time'
+		sys.exit()
+	return start, end, time_step, mesh_int, alpha, initial_conditions
+
+
+def set_up_model(start, end, time_step, mesh_int, alpha, initial_conditions, verbose = 'false'):
+	#create a simple testcase
+	model = SedimentModel()
+	mesh = UnitSquareMesh(mesh_int,mesh_int)
+	model.set_mesh(mesh)
+	try:
+		init_cond = Expression(initial_conditions) # simple slope
+	except:
+		print 'Your initial condition was not a function'
+		sys.exit()
+	init_sed = Expression('x[0]') # this gives
+	# total of above gives a slope of 0 to 2 (over the unit square)
+	model.set_initial_conditions(init_cond,init_sed)
+	model.set_start_time(start)
+	model.set_timestep(time_step)
+	model.set_end_time(end)
+	model.set_diffusion_coeff(alpha)
+	model.init()
+	model.solve()
+	# answer should be 1 everywhere
+	plot(model.get_total_height(),interactive=True)
+	print model.get_total_height_array()
+	answer = model.get_total_height_array()
+	return answer
+
+
+if __name__ == "__main__" :
+
+	fname , verbose = Main()
+	start, end, time_step, mesh_int, alpha, initial_conditions = Parameters(fname)
+	
+	set_up_model(start, end, time_step, mesh_int, alpha, initial_conditions, verbose)
+
+
+
+
+
+
+
+
 
